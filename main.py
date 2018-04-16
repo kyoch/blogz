93342066e53f87659403303951bd63cdaf379365
Kiyeon Yoch <kkyoch@gmail.com>
2018-04-15 19:09:43 -0500

    Completed Build a Blog

--- main.py
+++ main.py
@@ -13,49 +13,61 @@ class Blog(db.Model):
     title = db.Column(db.String(200))
     body = db.Column(db.String(2000))
 
-    def __init__(self, title):
+    def __init__(self, title, body):
         self.title = title
         self.body = body
 
 
-@app.route('/', methods=['POST','GET'])
-def index():
+@app.route('/blog')
+def getBlogs():
 
-    title = request.form['title']
-    body = request.form['body']
+    entry_id = request.args.get('id')
+
+    if (entry_id):
+        post = Blog.query.get(entry_id)
+        return render_template('singleblog.html', post=post)
+
+    else:
+        posts = Blog.query.all()       
+        return render_template('blog.html', posts=posts) 
+
+
+
+@app.route('/newpost', methods=['POST','GET'])
+def addBlog(): 
+    
     title_error = ''
     text_error = ''
 
     if request.method == 'POST':
         title = request.form['title']
-        new_title = Blog(title)
-        db.session.add(new_title)
-        db.session.commit()
-
-    if request.method == 'POST':
-        text = request.form['body']
-        new_text = Blog(text)
-        db.session.add(new_text)
-        db.session.commit()
-
-    if len(title) == 0:
-        title_error = "Please enter your blog title."
-
-    if len(body) == 0:
-        text_error = "Please enter your blog content."
-    
-
-    posts = Blog.query.all()
-
-    if len(title_error) == 0 and len(text_error) == 0:     
-        return render_template('index.html', title="Build a Blog", posts=posts)
-
-    else:
-        return render_template('addnew.html', title_error=title_error, text_error=text_error) 
-
-    
-
-
+        body = request.form['body']  
         
+        if title.strip() == "":
+            title_error = "Please enter your blog title."
+            title = ''
+
+        if body.strip() == "":
+            text_error = "Please enter your blog content."       
+            body = ''
+
+        if (title == "") or (body == ""):
+            return render_template('addnew.html', title=title, body=body, title_error=title_error, text_error=text_error)
+        
+        else:
+            new_entry = Blog(title, body)
+            db.session.add(new_entry)
+            db.session.commit()
+            #posts = Blog.query.all()
+            
+            url = "/blog?id=" + str(new_entry.id)
+            #return render_template('blog.html', title="Build a Blog", posts=posts)
+            #return redirect('/blog')
+            return redirect(url)
+    else:
+         return render_template('addnew.html')   
+    
+
+      
 if __name__ == '__main__':
     app.run()
